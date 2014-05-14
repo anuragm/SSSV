89fc6911adf1c6d3e23fbc79d72721218086a45e
Anurag Mishra <anurag@wireless-plus-058-037.usc.edu>
2014-05-14 12:54:38 -0700

    Initial Commit

---
+++ SSSV/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SSSV
+//
+//  Created by Anurag Mishra on 5/14/14.
+//  Copyright (c) 2014 Anurag Mishra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
